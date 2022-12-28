df8495e9f0cb2c06ca8e62883434459ca9c8c390
Hessa <haalmheiri99@gmail.com>
2022-12-28 14:51:51 +0400

    changes in notification view and stopping audio using pending intent

--- app/src/main/java/com/example/foregroundserviceapp/MainActivity.kt
+++ app/src/main/java/com/example/foregroundserviceapp/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.foregroundserviceapp
 
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.widget.RemoteViews
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.foregroundserviceapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity() {
+    private val CHANNEL_ID = "ForegroundServiceChannel"
     private lateinit var binding: ActivityMainBinding
     private var serviceIsOn = false
     private var playList: ArrayList<SongModel>? = null
     private var currentSongPosition = 0
+    var serviceIntent : Intent? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val runningServices = activityManager.getRunningServices(Int.MAX_VALUE)
+
+        val isServiceRunning = runningServices.any { service ->
+            service.service.className == MyService::class.java.name
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -51,25 +66,39 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
+
     private fun startService() {
+
         serviceIsOn = true
         binding.ivPlayPause.setImageResource(R.drawable.ic_pause)
-        val serviceIntent = Intent(this, MyService::class.java)
+         serviceIntent = Intent(this, MyService::class.java)
        // serviceIntent.putExtra("AudioON", "Audio is playing")
-        serviceIntent.putExtra("SongResource", playList?.get(currentSongPosition)?.getResourceId())
-        serviceIntent.putExtra("SongName", playList?.get(currentSongPosition)?.getName())
-        ContextCompat.startForegroundService(this, serviceIntent)
+        serviceIntent!!.putExtra("SongResource", playList?.get(currentSongPosition)?.getResourceId())
+        serviceIntent!!.putExtra("SongName", playList?.get(currentSongPosition)?.getName())
+
+          ContextCompat.startForegroundService(this, serviceIntent!!)
+
     }
 
     private fun stopService() {
         serviceIsOn = false
         binding.ivPlayPause.setImageResource(R.drawable.ic_play)
-        val serviceIntent = Intent(this, MyService::class.java)
+         serviceIntent = Intent(this, MyService::class.java)
         stopService(serviceIntent)
     }
 
     private fun displaySongView(currentSongPosition: Int) {
         binding.tvSongName.text = playList?.get(currentSongPosition)?.getName()
         binding.ivSongImage.setImageResource(playList?.get(currentSongPosition)!!.getImage())
+    }
+
+
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+
     }
 }
--- app/src/main/java/com/example/foregroundserviceapp/MyService.kt
+++ app/src/main/java/com/example/foregroundserviceapp/MyService.kt
@@ -2,58 +2,92 @@ package com.example.foregroundserviceapp
 
 import android.app.*
 import android.app.PendingIntent.FLAG_IMMUTABLE
-import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
-import android.net.Uri
+import android.media.session.PlaybackState.ACTION_STOP
 import android.os.Build
 import android.os.IBinder
+import android.widget.RemoteViews
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import java.io.IOException
+import com.example.foregroundserviceapp.databinding.ActivityMainBinding
 
 class MyService : Service() {
 
     private val CHANNEL_ID = "ForegroundServiceChannel"
     private lateinit var mediaPlayer: MediaPlayer
 
+
     override fun onCreate() {
         super.onCreate()
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        Toast.makeText(this, "service is on", Toast.LENGTH_SHORT).show()
-        //val input = intent.getStringExtra("AudioON")
-        val songResource = intent.getIntExtra("SongResource", 0)
-        val songName = intent.getStringExtra("SongName")
 
-        createNotificationChannel()
+        val stopIntent = Intent(this, MyService::class.java).apply {
+            action = ACTION_STOP.toString()
+        }
+        val stopPendingIntent: PendingIntent = PendingIntent.getService(this, 0, stopIntent, FLAG_IMMUTABLE)
+
+        if (intent != null){
+            when(intent.action){
+                ACTION_STOP.toString() ->{stopSound()}
+            }
+        }
 
         // show a notification in the notification bar
         val notificationIntent = Intent(this, MainActivity::class.java)
+
         val pendingIntent = PendingIntent.getActivity(
             this,
             0, notificationIntent, FLAG_IMMUTABLE
         )
+                // terminate music and stop service
+
+
+
+       val notificationLayout = RemoteViews(packageName, R.layout.notification_view)
+        Toast.makeText(this, "service is on", Toast.LENGTH_SHORT).show()
+        //val input = intent.getStringExtra("AudioON")
+        val songResource = intent.getIntExtra("SongResource", 0)
+        val songName = intent.getStringExtra("SongName")
+        notificationLayout.setTextViewText(R.id.tvContent,"Audio Name: $songName")
+        notificationLayout.setOnClickPendingIntent(R.id.btnStop,stopPendingIntent)
+        mediaPlayer = MediaPlayer.create(this, songResource)
+        mediaPlayer.isLooping = true
+//
+        createNotificationChannel()
+//
+
 
         val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Music is Playing")
-            .setContentText("$songName is playing")
+           .setStyle(NotificationCompat.DecoratedCustomViewStyle())
+            .setCustomContentView(notificationLayout)
+           // .setContentTitle("Music is Playing")
+           // .setContentText("$songName is playing")
             .setSmallIcon(R.drawable.ic_music)
             .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            //.addAction(0, "Stop Music",stopMusicIntent )
             .build()
+//
         startForeground(1, notification)
 
+        // creating a mediaplayer
+
+        playSound()
+
         // do heavy work on a background thread
-        val thread = Thread(
-            Runnable {
-                mediaPlayer = MediaPlayer.create(this, songResource)
-                mediaPlayer.isLooping = true
-                mediaPlayer.start()
-                Thread.sleep(1000)
-            }
-        )
-        thread.start()
+        //  val thread = Thread(
+        // Runnable {
+        //   mediaPlayer = MediaPlayer.create(this, songResource)
+        //   mediaPlayer.isLooping = false
+        //   mediaPlayer.start()
+        //      Thread.sleep(1000)
+        //   }
+        //  )
+        //  thread.start()
 
         return START_NOT_STICKY
     }
@@ -67,16 +101,36 @@ class MyService : Service() {
             )
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(serviceChannel)
+//
         }
     }
 
     override fun onDestroy() {
-        Toast.makeText(this, "service is off", Toast.LENGTH_SHORT).show()
-        mediaPlayer.stop()
         super.onDestroy()
+        Toast.makeText(this, "service is off", Toast.LENGTH_SHORT).show()
+        stopSound()
     }
+
 
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
+
+    // media functions
+    private fun playSound() {
+        // if (mediaPlayer == null) {
+        if (!mediaPlayer.isPlaying) {
+            mediaPlayer.start()
+        }
+
+        //  }
+    }
+
+    private fun stopSound() {
+        if (mediaPlayer.isPlaying) {
+            mediaPlayer.stop()
+            mediaPlayer!!.release()
+        }
+    }
 }
+
